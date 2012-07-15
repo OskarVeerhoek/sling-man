5a9101bdf162c5502bf12887d2a7095c4b4af094
Oskar Veerhoek <thecodinguniverse@gmail.com>
2012-07-15 11:10:48 +0200

    Added main file.

---
+++ Source/slingman/SlingMan.java
@@ -0,0 +1,10 @@
+package slingman;
+
+public class SlingMan {
+	public static void main(String args[]) {
+		// Check system resources 
+		// Check CPU speed
+		// Initialize random number generator
+		// TODO: Add more steps
+	}
+}
